d94b87316c0ee5f089a3ca93c42ee423cc480358
Arron M <marshall.arron@gmail.com>
2019-05-08 08:21:23 -0700

    Get FriendsList component rendered to screen

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FriendsList from './components/FriendsList/FriendsList';
+
+function App() {
+  return (
+    <div className="App">
+      <FriendsList />
+    </div>
+  );
+}
+
+export default App;
